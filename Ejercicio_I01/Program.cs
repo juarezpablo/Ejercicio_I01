3658ae12c0cd14e956037d474312cba439b13009
Pablo-notebook <juarezjpablo7@gmail.com>
2023-09-10 12:43:15 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio_I01/Program.cs
@@ -0,0 +1,18 @@
+﻿using Entidades;
+namespace Ejercicio_I01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Cuenta cuenta_nueva = new Cuenta("pablo", 100);
+            Console.WriteLine(cuenta_nueva.Mostrar());
+            cuenta_nueva.Retirar(50);
+            Console.WriteLine(cuenta_nueva.Mostrar());
+            cuenta_nueva.Ingresar(200);
+            Console.WriteLine(cuenta_nueva.Mostrar());
+        }
+    }
+}
---
+++ Entidades/Cuenta.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace Entidades
+{
+    public class Cuenta
+    {
+        private string titular;
+        private int cantidad;
+        public Cuenta(string titular, int cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string GetTitular()
+        {
+            return this.titular;
+        }
+
+        public int getCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb=new StringBuilder();
+            sb.AppendLine($"Titular: {this.titular}");
+            sb.AppendLine($"Cantidad: {this.cantidad}");
+            return sb.ToString();
+        }
+
+        public void Ingresar(int cantidad)
+        {
+            if(cantidad >=0)
+            {
+                this.cantidad=this.cantidad+cantidad;
+            }
+        }
+
+        public void Retirar(int monto)
+        {
+            this.cantidad = this.cantidad - monto;
+        }
+    }
+}
